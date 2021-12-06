cd2f24dc9fc21cb35c52d1b9add2249cdf1c6a12
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2021-12-06 15:24:19 +0600

    Update Tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,13 +18,17 @@ module.exports = {
             center: true,
             padding: "1rem",
             screens: {
+                sm: '640px',
+                md: '768px',
                 lg: "1124px",
                 xl: "1124px",
                 "2xl": "1124px",
 
-            }
+            },
         }
     },
+
+
     variants: {
         extend: {},
     },
