3de74e7e16565c18bfe59aef441e664d465ff47f
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2021-12-07 21:24:13 +0600

    Adding styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,55 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                "bookmark-purple": "#5267DF",
-                "bookmark-red": "#FA5959",
-                "bookmark-blue": "#242A45",
-                "bookmark-grey": "#9194A2",
-                "bookmark-white": "#f7f7f7",
+        purge: [],
+        darkMode: 'media', // or 'media' or 'class'
+        // darkMode: 'class', // or 'media' or 'class'
+        theme: {
+            extend: {
+                colors: {
+                    "bookmark-purple": "#5267DF",
+                    "bookmark-red": "#FA5959",
+                    "bookmark-blue": "#242A45",
+                    "bookmark-grey": "#9194A2",
+                    "bookmark-white": "#f7f7f7",
+                },
+            },
+            fontFamily: {
+                Poppins: ["Poppins,sans-serif"],
+            },
+            container: {
+                center: true,
+                padding: "1rem",
+                screens: {
+                    sm: '640px',
+                    md: '768px',
+                    lg: "1124px",
+                    xl: "1124px",
+                    "2xl": "1124px",
+
+                },
+            }
+        },
+
+
+        variants: {
+            extend: {
+                textColor: ['visited'],
+                backgroundColor: ['checked'],
+                borderColor: ['checked'],
+                // Dark Mode
+                textOpacity: ['dark'],
             },
         },
-        fontFamily: {
-            Poppins: ["Poppins,sans-serif"],
-        },
-        container: {
-            center: true,
-            padding: "1rem",
-            screens: {
-                sm: '640px',
-                md: '768px',
-                lg: "1124px",
-                xl: "1124px",
-                "2xl": "1124px",
+        plugins: [],
+    }
+    // const color = require('tailwind/colors')
 
-            },
-        }
-    },
+// module.exports = {
+//     theme: {
+//         colors: {
+//             gray: colors.coolGray,
+//             blue: colors.lightBlue,
+//             red: colors.rose,
+//         },
 
-
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+//     }
+// }
